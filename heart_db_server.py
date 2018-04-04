503476d8f1624b25853827d57485d917a3ca8a20
hagankristen <kristenhagan@DESKTOP-F6L9VU9.localdomain>
2018-04-04 14:39:15 -0400

    update server files

--- heart_db_server.py
+++ heart_db_server.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+
 from pymodm import connect, errors
 import models
 import datetime
@@ -8,7 +10,9 @@ from main import check_tachycardia, calculate_interval_avg
 
 
 app = Flask(__name__)
-connect("mongodb://vcm-3580.vm.duke.edu:27017/heart_rate_app")
+CORS(app)
+comp_with_db = "mongodb://vcm-3580.vm.duke.edu:27017/heart_rate_app"
+connect(comp_with_db)
 
 
 @app.route("/api/heart_rate", methods=["POST"])
@@ -78,7 +82,8 @@ def get_heart_rates(user_email):
     try:
         user = models.User.objects.raw({"_id": user_email}).first()
         hi = {"user_email": user_email,
-              "recorded heart rates": user.heart_rate
+              "recorded_heart_rates": user.heart_rate,
+              "hr_times": user.heart_rate_times
               }
         return jsonify(hi), 200
     except errors.DoesNotExist:
@@ -91,10 +96,21 @@ def get_heart_rate_average(user_email):
     try:
         user = models.User.objects.raw({"_id": user_email}).first()
         hi = {"user_email": user_email,
-              "average of recorded heart rates":
+              "average_recorded_heart_rates":
               sum(user.heart_rate)/len(user.heart_rate)
               }
         return jsonify(hi), 200
     except errors.DoesNotExist:
         data = {"message": 'User does not exist.'}
+        return jsonify(data), 400
+
+@app.route("/api/heart_rate/userlist", methods=["GET"])
+def get_userlist():
+    try:
+        db.getUsers()
+        hi = {"user_emails": 1,
+        }
+        return jsonify(hi), 200
+    except UnknownError:
+        data = {"message": 'Problem getting list.'}
         return jsonify(data), 400
